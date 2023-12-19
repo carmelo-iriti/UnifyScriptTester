294203312ac3fd882b1fc4a5a539af4fad579abd
Sourcepoint <carmelo@sourcepoint.com>
2023-12-19 16:55:53 +0100

    deleteCookie edit

--- src/stateManagement.js
+++ src/stateManagement.js
@@ -11,14 +11,12 @@ function clearLocalStorage() {
 }
 
 function clearAllCookies() {
-    var cookies = document.cookie.split(";");
+    const cookies = document.cookie.split(";");
 
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        // document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=" + document.domain;
+    for (let cookie of cookies) {
+        const cookieName = cookie.split('=')[0].trim();
+        document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=property-runner.000webhostapp.com';
+        document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=.000webhostapp.com';
     }
 }
 
